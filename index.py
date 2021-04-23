328a6604b31849b23c216b6df118cf5327433bcc
cbackstrom80 <curtis.backstrom@gmail.com>
2021-04-23 14:04:24 -0700

    update index.py

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import uwsgidecorators
+from splunk_otel.tracing import start_tracing
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 app = Flask(__name__)
+
+@uwsgidecorators.postfork
+def setup_tracing():
+    start_tracing()
+    # instrument our flask app instance eplicitly
+    FlaskInstrumentor().instrument_app(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
+    return "Hello, World!"
